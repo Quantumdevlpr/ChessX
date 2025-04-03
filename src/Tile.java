bd11bc28fb8a5a1f1bda6775ce6ef6ff67b9fbc4
shivansh <mshivanshmaurya@gmail.com>
2025-04-03 09:33:27 +0530

    Part 1

--- src/Tile.java
+++ src/Tile.java
@@ -16,12 +16,27 @@ public abstract class Tile {
         }
         @Override
         public boolean isTileOccupied(){
-
+            return false;
         }
 
         @Override
         public Piece getPiece(){
             return null;
+        }
+    }
+    public static final class OccupiedTile extends Tile {
+        Piece pieceOnTile;
+        OccupiedTile(int tileCoordinate , Piece pieceOnTile){
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+        @Override
+        public boolean isTileOccupied(){
+            return true;
+        }
+        @Override
+        public Piece getPiece(){
+            return pieceOnTile;
         }
     }
 }
